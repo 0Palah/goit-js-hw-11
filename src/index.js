3ab9d337dca30f2606e03f506517eb97782db26c
Oleg Palashynskyi <palah@ukr.net>
2022-09-16 17:26:06 +0300

    fin

--- src/index.js
+++ src/index.js
@@ -16,39 +16,63 @@ const pixabayApi = new PixabayApi();
 // console.log(pixabayApi);
 
 // Викликаємо при кліку на кнопку load-more
-const onLoadMoreBtnElClick = evt => {
+const onLoadMoreBtnElClick = async evt => {
   pixabayApi.page += 1;
-  pixabayApi
-    .fetchPhotosByQuery()
-    .then(response => {
-      // деструктуризую дані з response
-      const { data } = response;
-      console.log(
-        `${pixabayApi.page} <= ${data.totalHits / pixabayApi.per_page}`
+
+  try {
+    const { data } = await pixabayApi.fetchPhotosByQuery();
+    console.log(
+      `${pixabayApi.page} <= ${data.totalHits / pixabayApi.per_page}`
+    );
+    console.log(`pixabayApi.page - ${pixabayApi.page}`);
+    console.log(
+      `data.totalHits / pixabayApi.per_page - ${
+        data.totalHits / pixabayApi.per_page
+      }`
+    );
+    if (pixabayApi.page >= data.totalHits / pixabayApi.per_page) {
+      loadMoreEl.classList.add('is-hidden');
+      loadMoreEl.removeEventListener('click', onLoadMoreBtnElClick);
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
       );
-      console.log(`pixabayApi.page - ${pixabayApi.page}`);
-      console.log(
-        `data.totalHits / pixabayApi.per_page - ${
-          data.totalHits / pixabayApi.per_page
-        }`
-      );
-      if (pixabayApi.page >= data.totalHits / pixabayApi.per_page) {
-        loadMoreEl.classList.add('is-hidden');
-        loadMoreEl.removeEventListener('click', onLoadMoreBtnElClick);
-        Notiflix.Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-      // додаємо до відмальованих картки через хенделбарс
-      galleryListEl.insertAdjacentHTML('beforeend', galleryCard(data.hits));
-    })
-    .catch(err => {
-      console.log(err);
-    });
+    }
+    // додаємо до відмальованих картки через хенделбарс
+    galleryListEl.insertAdjacentHTML('beforeend', galleryCard(data.hits));
+  } catch (err) {
+    console.log(err);
+  }
+  // pixabayApi
+  //   .fetchPhotosByQuery()
+  //   .then(response => {
+  //     // деструктуризую дані з response
+  //     const { data } = response;
+  //     console.log(
+  //       `${pixabayApi.page} <= ${data.totalHits / pixabayApi.per_page}`
+  //     );
+  //     console.log(`pixabayApi.page - ${pixabayApi.page}`);
+  //     console.log(
+  //       `data.totalHits / pixabayApi.per_page - ${
+  //         data.totalHits / pixabayApi.per_page
+  //       }`
+  //     );
+  //     if (pixabayApi.page >= data.totalHits / pixabayApi.per_page) {
+  //       loadMoreEl.classList.add('is-hidden');
+  //       loadMoreEl.removeEventListener('click', onLoadMoreBtnElClick);
+  //       Notiflix.Notify.failure(
+  //         "We're sorry, but you've reached the end of search results."
+  //       );
+  //     }
+  //     // додаємо до відмальованих картки через хенделбарс
+  //     galleryListEl.insertAdjacentHTML('beforeend', galleryCard(data.hits));
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 };
 
 // Викликаємо при сабміті, прослуховуємо Форму
-const onsearchFormElSubmit = evt => {
+const onsearchFormElSubmit = async evt => {
   evt.preventDefault();
 
   loadMoreEl.classList.add('is-hidden');
@@ -61,35 +85,62 @@ const onsearchFormElSubmit = evt => {
   pixabayApi.page = 1;
 
   // Викликаємо метод екземпляру для запиту на сервер
-  pixabayApi
-    .fetchPhotosByQuery()
-    .then(response => {
-      // деструктуризую дані з response
-      const { data } = response;
-      console.log(response);
-      if (data.totalHits === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-        return;
-      }
-      if (pixabayApi.page >= data.totalHits / pixabayApi.per_page) {
-        // Якщо ТІЛЬКИ одна сторінка то тільки відмальовуємо, (is-hidden не знімаємо)
-        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        return (galleryListEl.innerHTML = galleryCard(data.hits));
-      }
-      // відмальовую картки через хенделбарс
-      galleryListEl.innerHTML = galleryCard(data.hits);
-      // показати кнопку
-      loadMoreEl.classList.remove('is-hidden');
-      // прослуховування на кнопку load-more
-      loadMoreEl.addEventListener('click', onLoadMoreBtnElClick);
+  try {
+    const { data } = await pixabayApi.fetchPhotosByQuery();
 
+    if (data.totalHits === 0) {
+      galleryListEl.innerHTML = '';
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    if (pixabayApi.page >= data.totalHits / pixabayApi.per_page) {
+      // Якщо ТІЛЬКИ одна сторінка то тільки відмальовуємо, (is-hidden не знімаємо)
       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      return (galleryListEl.innerHTML = galleryCard(data.hits));
+    }
+    // відмальовую картки через хенделбарс
+    galleryListEl.innerHTML = galleryCard(data.hits);
+    // показати кнопку
+    loadMoreEl.classList.remove('is-hidden');
+    // прослуховування на кнопку load-more
+    loadMoreEl.addEventListener('click', onLoadMoreBtnElClick);
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  } catch (err) {
+    console.log(err);
+  }
+
+  // Викликаємо метод екземпляру для запиту на сервер
+  // pixabayApi
+  //   .fetchPhotosByQuery()
+  //   .then(response => {
+  //     // деструктуризую дані з response
+  //     const { data } = response;
+  //     console.log(response);
+  //     if (data.totalHits === 0) {
+  //       Notiflix.Notify.failure(
+  //         'Sorry, there are no images matching your search query. Please try again.'
+  //       );
+  //       return;
+  //     }
+  //     if (pixabayApi.page >= data.totalHits / pixabayApi.per_page) {
+  //       // Якщо ТІЛЬКИ одна сторінка то тільки відмальовуємо, (is-hidden не знімаємо)
+  //       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  //       return (galleryListEl.innerHTML = galleryCard(data.hits));
+  //     }
+  //     // відмальовую картки через хенделбарс
+  //     galleryListEl.innerHTML = galleryCard(data.hits);
+  //     // показати кнопку
+  //     loadMoreEl.classList.remove('is-hidden');
+  //     // прослуховування на кнопку load-more
+  //     loadMoreEl.addEventListener('click', onLoadMoreBtnElClick);
+
+  //     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
 
   //   console.log(pixabayApi);
 };
